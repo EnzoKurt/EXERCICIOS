9bfa04c323aefacf3c78207015919f19efa3f51f
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2023-08-30 21:55:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double sm,qtd,kw;
+Console.WriteLine("Digite o salário mínimo: ");
+sm= double.Parse(Console.Readline());
+Console.WriteLine("Digite a quantidade gasta de kw: ");
+qtd= double.Parse(Console.Readline());
+kw= (sm/7)/100;
+Console.WriteLine("O valor em reais, de cada kw é {0} ", kw);
